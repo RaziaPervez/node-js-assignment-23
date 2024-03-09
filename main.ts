863482f73a89445ae3bf18af7e3b2e10097f3135
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-09 10:43:27 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,34 @@
+//project=23
+
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+let car="sabaru"
+//console.log("is car== "sabaru" =? I predict to otherwise false)
+console.log(car==`sabaru`)
+console.log(car==`civic`)
+
+let name1="Nadia"
+//console.log("is name== "Nadia" =? I predict to otherwise false)
+console.log(name1==`Nadia`)
+console.log(name1==`Razia`)
+
+let mountain="Mount Everest"
+//console.log("is mountain== "Mount Everest" =? I predict to otherwise false)
+console.log(mountain==`Mount Everest`)
+console.log(car==`k2`)
+
+let bike="superbike"
+//console.log("is bike== "superbike" =? I predict to otherwise false)
+console.log(bike==`superbike`)
+console.log(bike==`normalbike`)
+
+let vegetable="carrot"
+//console.log("is vegetable== "carrot" =? I predict to otherwise false)
+console.log(vegetable==`carrot`)
+console.log(car==`tomato`)
